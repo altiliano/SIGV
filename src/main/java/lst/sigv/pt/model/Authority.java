47c045d67c0f2cc1f4599a30ecbfc0f62f5983c7
altiliano <altilianofonseca39@@gmail.com>
2020-11-13 17:56:47 +0000

    add user Authority close #2

---
+++ src/main/java/lst/sigv/pt/model/Authority.java
@@ -0,0 +1,22 @@
+package lst.sigv.pt.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by Afonseca on 13/11/20
+ */
+@Setter
+@Getter
+@Entity
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String role;
+    @ManyToMany( mappedBy = "authorities")
+    private Set<UserEntity> users;
+}
