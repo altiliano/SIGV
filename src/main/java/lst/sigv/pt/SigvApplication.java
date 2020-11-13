af6d554acba123f2ee204788d340fba119706c0a
fonseca <altilianofonseca@gmail.com>
2020-11-13 16:16:57 +0000

    first commit

---
+++ src/main/java/lst/sigv/pt/SigvApplication.java
@@ -0,0 +1,13 @@
+package lst.sigv.pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lst/sigv/pt/SigvApplicationTests.java
@@ -0,0 +1,13 @@
+package lst.sigv.pt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
