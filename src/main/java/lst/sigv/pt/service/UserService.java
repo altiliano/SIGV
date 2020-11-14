1f2154ccb1f2a4df36b6d660fbde6c12eb4c8852
fonseca <altilianofonseca@gmail.com>
2020-11-14 00:04:05 +0000

    add UserService close #3

---
+++ src/main/java/lst/sigv/pt/service/UserService.java
@@ -0,0 +1,10 @@
+package lst.sigv.pt.service;
+
+import lst.sigv.pt.model.UserEntity;
+
+/**
+ * Created by Afonseca on 13/11/20
+ */
+public interface UserService {
+    UserEntity findUserByUsername(String username);
+}
---
+++ src/main/java/lst/sigv/pt/service/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package lst.sigv.pt.service;
+
+import lst.sigv.pt.model.UserEntity;
+import lst.sigv.pt.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Afonseca on 13/11/20
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserEntity findUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElse( null);
+    }
+}
