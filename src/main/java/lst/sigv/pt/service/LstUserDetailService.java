ff0b5e74a9a7d69e7dabc2aa50aab4c75a9b46d1
altiliano <altilianofonseca39@@gmail.com>
2020-11-14 00:04:36 +0000

    Setup Spring Security and JWT token close #1

---
+++ src/main/java/lst/sigv/pt/service/LstUserDetailService.java
@@ -0,0 +1,47 @@
+package lst.sigv.pt.service;
+
+import lombok.RequiredArgsConstructor;
+import lst.sigv.pt.model.Authority;
+import lst.sigv.pt.model.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Afonseca on 13/11/20
+ */
+@RequiredArgsConstructor
+@Service
+public class LstUserDetailService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userService.findUserByUsername(username);
+        if (userEntity == null) {
+            throw new UsernameNotFoundException("user with username: " + username + " not found");
+        }
+        return new org.springframework.security.core.userdetails.User(userEntity.getUsername(), userEntity.getPassword(), convertSpringAuthorities(userEntity.getAuthorities()));
+    }
+
+    private Collection<? extends GrantedAuthority> convertSpringAuthorities(Set<Authority> authorities) {
+        if (authorities != null && authorities.size() > 0) {
+            return authorities.stream()
+                    .map(Authority::getRole)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toSet());
+        }
+        return new HashSet<>();
+    }
+}
