ceeb87ebfbef2fe187d9309716c9644865f4c1b8
fonseca <altilianofonseca@gmail.com>
2020-11-16 17:51:53 +0000

    Add Plane Entity close #10

---
+++ src/main/java/lst/sigv/pt/model/PlaneEntity.java
@@ -0,0 +1,29 @@
+package lst.sigv.pt.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by Afonseca on 16/11/20
+ */
+@Getter
+@Setter
+@Entity(name = "planes")
+public class PlaneEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String registration;
+    private String photoUrl;
+    private String textureUrl;
+    private PlaneStatus status;
+    private String aircraftType;
+    @OneToMany(mappedBy = "plane")
+    private Set<BookingEntity> bookings;
+    @ManyToMany(mappedBy = "planes")
+    private Set<RouteEntity> routes;
+}
