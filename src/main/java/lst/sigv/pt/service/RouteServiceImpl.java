e25ae12847d911f1bad4990b81caa58fee336028
fonseca <altilianofonseca@gmail.com>
2020-11-26 23:48:13 +0000

    refactoring RouteService

--- src/main/java/lst/sigv/pt/service/RouteServiceImpl.java
+++ src/main/java/lst/sigv/pt/service/RouteServiceImpl.java
@@ -1,8 +1,6 @@
 package lst.sigv.pt.service;
 
-import lst.sigv.pt.exception.InvalidPlaneStatusException;
 import lst.sigv.pt.exception.InvalidRouteStatusException;
-import lst.sigv.pt.model.PlaneEntity;
 import lst.sigv.pt.model.RouteEntity;
 import lst.sigv.pt.model.RouteStatus;
 import lst.sigv.pt.model.api.RestRoute;
@@ -43,7 +41,7 @@ public class RouteServiceImpl implements RouteService {
     @Override
     public RestRoute activeRoute(String routeId) {
         RouteEntity entity = getRouteById(routeId);
-        if (!isValidStatus(entity.getStatus(), RouteStatus.ACTICE)) {
+        if (isNotValidStatus(entity.getStatus(), RouteStatus.ACTICE)) {
             throw new InvalidRouteStatusException("Can not active route with id: " + routeId);
         }
         entity.setStatus(RouteStatus.ACTICE);
@@ -53,7 +51,7 @@ public class RouteServiceImpl implements RouteService {
     @Override
     public RestRoute inactiveRoute(String routeId) {
         RouteEntity routeEntity = getRouteById(routeId);
-        if (!isValidStatus(routeEntity.getStatus(), RouteStatus.INACTIVE)) {
+        if (isNotValidStatus(routeEntity.getStatus(), RouteStatus.INACTIVE)) {
             throw new InvalidRouteStatusException("Can not inactive route with id: " + routeId);
         }
         routeEntity.setStatus(RouteStatus.INACTIVE);
@@ -81,12 +79,13 @@ public class RouteServiceImpl implements RouteService {
 
     @Override
     public void deleteRoute(String routeId) {
-        RouteEntity routeEntity = routeRepository.findById(Long.valueOf(routeId)).orElse(null);
-        if (!isValidStatus(routeEntity.getStatus(), RouteStatus.DELETE)) {
+        RouteEntity routeEntity = getRouteById(routeId);
+        if (isNotValidStatus(routeEntity.getStatus(), RouteStatus.DELETE)) {
             throw new InvalidRouteStatusException("can not delete route with id: " + routeId);
         }
         routeEntity.setStatus(RouteStatus.DELETE);
         routeRepository.save(routeEntity);
+
     }
 
     @Override
@@ -94,14 +93,10 @@ public class RouteServiceImpl implements RouteService {
         return routeMapper.routeEntityToRestRoute(getRouteById(routeId));
     }
 
-    public boolean isValidStatus(RouteStatus currentStatus, RouteStatus newRouteStatus) {
+    public boolean isNotValidStatus(RouteStatus currentStatus, RouteStatus newRouteStatus) {
         if (currentStatus.equals(RouteStatus.DELETE)) {
-            return false;
-        } else {
-            if (currentStatus.equals(newRouteStatus)) {
-                return false;
-            }
+            return true;
         }
-        return true;
+        return currentStatus.equals(newRouteStatus);
     }
 }
