8b5f4f495b31bb7d386619fdba0346593d17cb85
altiliano <altilianofonseca39@gmail.com>
2021-04-11 01:43:05 +0100

    ability change password close #28

---
+++ src/main/java/lst/sigv/pt/model/api/RestChangePasswordForm.java
@@ -0,0 +1,26 @@
+package lst.sigv.pt.model.api;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Getter;
+import lombok.Setter;
+import lst.sigv.pt.validator.FieldsValueMatch;
+
+@Getter
+@Setter
+@FieldsValueMatch(field = "newPassword", fieldMatch = "confirmPassword", message = "{password.fieldsValueMatch}")
+public class RestChangePasswordForm implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotEmpty(message = "{password.notEmpty}")
+    private String currentPassword;
+    @NotEmpty(message = "{password.notEmpty}")
+    @NotEmpty(message = "{newPassword.notEmpty}")
+    private String newPassword;
+    @NotEmpty(message = "{confirmPassword.notEmpty}")
+    private String confirmPassword;
+
+}
