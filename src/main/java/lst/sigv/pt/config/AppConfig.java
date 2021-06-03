ca7f9e15e1284f5b968b361b941fe65d828c8769
afonseca <altilianofonseca39@gmail.com>
2021-06-03 20:20:08 +0100

    fix spring boot Reject: HTTP 'PUT' is not allowed on @PutMapping

--- src/main/java/lst/sigv/pt/config/AppConfig.java
+++ src/main/java/lst/sigv/pt/config/AppConfig.java
@@ -45,7 +45,8 @@ public class AppConfig extends WebMvcConfigurationSupport {
 
     @Override
     protected void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+            .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
 
     @Override
